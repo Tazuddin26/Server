1f4ccad4d382b91186d90654d0f0e8121be1fa10
Tazuddin <tazuddin26@gmail.com>
2022-12-03 12:49:07 +0600

    JSON Server

--- index.js
+++ index.js
@@ -1,21 +1,38 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 3001
 const fs = require("fs")
+
 
 app.get('/', (req, res) => {
   res.send('Bismillah')
 })
-app.get('/start', (req, res) => {
-  fs.writeFile("Count",'0', () => {})
-  res.send('Bismillah')
+app.get('/employees', (req, res) => {
+  fs.readFile("database",'utf8', (err,data) => {
+    const allData = JSON.parse(data)
+  res.send(JSON.stringify(allData.employees)) //(allData-Object & employees - Property)
+  })
 })
-app.get('/num', (req, res) => {
-  fs.readFile("Count",'utf8', (err,data) => {
-    const x = parentInt(data)+1
-    const text = x.toString()
-    fs.writeFile('Count',text, () => {})
-    res.send("Count Increased")
+app.get('/depertments', (req, res) => {
+  fs.readFile("database",'utf8', (err,data) => {
+    const allData = JSON.parse(data)
+  res.send(JSON.stringify(allData.depertments)) //(allData-Object & employees - Property)
+  })
+})
+app.get('/create-employee/:name', (req, res) => {
+  fs.readFile("database",'utf8', (err,data) => {
+    const allData = JSON.parse(data)
+ allData.employees.push(req.params.name)
+ fs.writeFile('database',JSON.stringify(allData), () => {})
+ res.send(`${req.params.name} added`)
+  })
+})
+app.get('/create-depertment/:name', (req, res) => {
+  fs.readFile("database",'utf8', (err,data) => {
+    const allData = JSON.parse(data)
+ allData.depertments.push(req.params.name)
+ fs.writeFile('database',JSON.stringify(allData), () => {})
+ res.send(`${req.params.name} added`)
   })
 })
 app.listen(port, () => {
