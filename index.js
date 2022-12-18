3dbd495a6118606879923c53d83d3af1be840507
Tazuddin <tazuddin26@gmail.com>
2022-12-18 21:51:45 +0600

    gitignore

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3001
 const fs = require("fs")
+const { parse } = require('path')
 const bodyParser = require('body-parser');
 const cors = require("cors");
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -10,10 +11,6 @@ app.use(cors({
   origin : 'http://localhost:3000',
   credentials: true, // <= Accept credentials (cookies) sent by the client
 }));
-
-app.get('/', (req, res) => {
-  res.send('Bismillah')
-})
 app.get('/employee', (req, res) => {
   fs.readFile("database",'utf8',(err, data) => {
     const allData = JSON.parse(data)
@@ -25,29 +22,70 @@ app.put('/employee/:id', (req, res) => {
     const allData = JSON.parse(data)
     const employeeinfoByID = allData.employee.find(x =>x.id==req.params.id);
     employeeinfoByID.name = req.body.name;
-    allData.employee.push(employeeinfoByID)
-    fs.writeFile('employee', JSON.stringify(allData), () => { })
+    fs.writeFile('database', JSON.stringify(allData), () => { })
     res.send(JSON.stringify(employeeinfoByID)) //(allData-Object & employees - Property)
   })
 })
-app.post('/create-employee', (req, res) => {
-  fs.readFile("database", 'utf8', (err, data) => {
+app.get('/companys', (req, res) => {
+  fs.readFile("companydata",'utf8',(err, data) => {
     const allData = JSON.parse(data)
-    const employeeData = req.body;
-    employeeData.id = allData.employee.length + 1;
-    allData.employee.push(employeeData)
-    fs.writeFile('database', JSON.stringify(allData), () => { })
-    res.send(`${req.body.name} added`)
+    res.send(JSON.stringify(allData.companys)) //(allData-Object & employees - Property)
   })
 })
-app.get('/create-department/:name', (req, res) => {
-  fs.readFile("departDatabase", 'utf8', (err, data) => {
+app.post('/create-company', (req, res) => {
+  fs.readFile("companydata", 'utf8', (err, data) => {
     const allData = JSON.parse(data)
-    allData.department.push(req.params.name)
-    fs.writeFile('departDatabase', JSON.stringify(allData), () => { })
-    res.send(`${req.params.name} added`)
+    const companysData = req.body;
+    companysData.id = allData.companys.length + 1;
+    allData.companys.push(companysData)
+    fs.writeFile('companydata', JSON.stringify(allData), () => { })
+    res.send(`${req.body.name}`)
   })
 })
+app.post("/signin", (req, res) => {
+  fs.readFile("database", "utf8", (err, data) => {
+      const allData = JSON.parse(data);
+      const userData = req.body;
+      const fusers = allData.users
+          .filter(user => user.username === userData.username && user.password === userData.password)
+      if (fusers.length > 0) {
+          res.send(JSON.stringify(fusers[0]));
+      }
+      else {
+          res.status(400).send()
+      }
+  });
+})
+app.post("/signup", (req, res) => {
+  fs.readFile("database", "utf8", (err, data) => {
+      const allData = JSON.parse(data);
+      const userData = req.body;
+      userData.id = allData.users.length + 1;
+      allData.users.push(userData);
+      fs.writeFile("database", JSON.stringify(allData), () => { });
+      res.send(JSON.stringify(userData));
+  });
+});
+app.get('/registeredStudents', (req, res) => {
+    fs.readFile("imagestore", 'utf8', (err, data) => {
+        const allData = JSON.parse(data)
+        res.send(JSON.stringify(allData.students))
+    })
+})
+app.post("/studentRegistration", (req, res) => {
+    fs.readFile("imagestore", "utf8", (err, data) => {
+        const allData = JSON.parse(data);
+        const reqData = req.body;
+        const rawImageString = reqData.image.replace(/^data:image\/jpeg;base64,/, "");
+        const buffer = Buffer.from(rawImageString, "base64");
+        reqData.id = allData.students.length + 1;
+        fs.writeFile(`public/student/${reqData.id}.jpeg`, buffer, () => { });
+        reqData.image = `${reqData.id}.jpeg`;
+        allData.students.push(reqData);
+        fs.writeFile("imagestore", JSON.stringify(allData), () => { });
+        res.send(JSON.stringify(reqData));
+    });
+});
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
