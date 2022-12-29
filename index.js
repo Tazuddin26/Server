ab4b1981bc9e1715547fdba21615af8a610ebf1e
Tazuddin <tazuddin26@gmail.com>
2022-12-29 11:02:04 +0600

    Local Server to fontend

--- index.js
+++ index.js
@@ -5,86 +5,121 @@ const fs = require("fs")
 const { parse } = require('path')
 const bodyParser = require('body-parser');
 const cors = require("cors");
+const { json } = require('express');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json())
+app.use(express.static("public"))
 app.use(cors({
-  origin : 'http://localhost:3000',
+  origin: 'http://localhost:3000',
   credentials: true, // <= Accept credentials (cookies) sent by the client
 }));
-app.get('/employee', (req, res) => {
-  fs.readFile("database",'utf8',(err, data) => {
-    const allData = JSON.parse(data)
-    res.send(JSON.stringify(allData.employee)) //(allData-Object & employees - Property)
-  })
-})
-app.put('/employee/:id', (req, res) => {
+app.put('/employee', (req, res) => {
   fs.readFile("database", 'utf8', (err, data) => {
     const allData = JSON.parse(data)
-    const employeeinfoByID = allData.employee.find(x =>x.id==req.params.id);
+    const employeeinfoByID = allData.employees.find(x => x.id == req.params.id);
     employeeinfoByID.name = req.body.name;
     fs.writeFile('database', JSON.stringify(allData), () => { })
     res.send(JSON.stringify(employeeinfoByID)) //(allData-Object & employees - Property)
   })
 })
 app.get('/companys', (req, res) => {
-  fs.readFile("companydata",'utf8',(err, data) => {
+  fs.readFile("companydata", 'utf8', (err, data) => {
     const allData = JSON.parse(data)
-    res.send(JSON.stringify(allData.companys)) //(allData-Object & employees - Property)
+    res.send(JSON.stringify(allData.profile))
   })
 })
-app.post('/create-company', (req, res) => {
+app.post('/company', (req, res) => {
   fs.readFile("companydata", 'utf8', (err, data) => {
     const allData = JSON.parse(data)
     const companysData = req.body;
-    companysData.id = allData.companys.length + 1;
-    allData.companys.push(companysData)
+    companysData.id = allData.profile.length + 1;
+    allData.profile.push(companysData)
     fs.writeFile('companydata', JSON.stringify(allData), () => { })
-    res.send(`${req.body.name}`)
+    res.send(JSON.stringify(companysData));
   })
 })
 app.post("/signin", (req, res) => {
   fs.readFile("database", "utf8", (err, data) => {
-      const allData = JSON.parse(data);
-      const userData = req.body;
-      const fusers = allData.users
-          .filter(user => user.username === userData.username && user.password === userData.password)
-      if (fusers.length > 0) {
-          res.send(JSON.stringify(fusers[0]));
-      }
-      else {
-          res.status(400).send()
-      }
+    const allData = JSON.parse(data);
+    const userData = req.body;
+    const fusers = allData.users
+      .filter(user => user.username === userData.username && user.password === userData.password)
+    if (fusers.length > 0) {
+      res.send(JSON.stringify(fusers[0]));
+    }
+    else {
+      res.status(400).send()
+    }
   });
 })
 app.post("/signup", (req, res) => {
   fs.readFile("database", "utf8", (err, data) => {
-      const allData = JSON.parse(data);
-      const userData = req.body;
-      userData.id = allData.users.length + 1;
-      allData.users.push(userData);
-      fs.writeFile("database", JSON.stringify(allData), () => { });
-      res.send(JSON.stringify(userData));
+    const allData = JSON.parse(data);
+    const userData = req.body;
+    userData.id = allData.users.length + 1;
+    allData.users.push(userData);
+    fs.writeFile("database", JSON.stringify(allData), () => { });
+    res.send(JSON.stringify(userData));
   });
 });
-app.get('/registeredStudents', (req, res) => {
-    fs.readFile("imagestore", 'utf8', (err, data) => {
-        const allData = JSON.parse(data)
-        res.send(JSON.stringify(allData.students))
-    })
+
+app.get('/employees', (req, res) => {
+  fs.readFile("database2", 'utf8', (err, data) => {
+    const mainData = JSON.parse(data)
+    res.send(JSON.stringify(mainData.abc))
+  })
 })
-app.post("/studentRegistration", (req, res) => {
-    fs.readFile("imagestore", "utf8", (err, data) => {
-        const allData = JSON.parse(data);
-        const reqData = req.body;
-        const rawImageString = reqData.image.replace(/^data:image\/jpeg;base64,/, "");
-        const buffer = Buffer.from(rawImageString, "base64");
-        reqData.id = allData.students.length + 1;
-        fs.writeFile(`public/student/${reqData.id}.jpeg`, buffer, () => { });
-        reqData.image = `${reqData.id}.jpeg`;
-        allData.students.push(reqData);
-        fs.writeFile("imagestore", JSON.stringify(allData), () => { });
-        res.send(JSON.stringify(reqData));
-    });
+app.post('/employee', (req, res) => {
+  fs.readFile("database2", 'utf8', (err, data) => {
+    const mainData = JSON.parse(data);
+    const reqData = req.body;
+    if (reqData.Employee_Name.length < 5) {
+      res.status(400).send(JSON.stringify({
+        error: "Name must be contain atleast 5 Characters"
+      }));
+      return;
+    }
+    const rawImageString = reqData.Image.replace(/^data:image\/jpeg;base64,/, "");
+    const buffer = Buffer.from(rawImageString, "base64");
+    reqData.id = mainData.abc.length + 1;
+    fs.writeFile(`public/employee/${reqData.id}.jpeg`, buffer, () => { });
+    reqData.Image = `${reqData.id}.jpeg`;
+    mainData.abc.push(reqData);
+    fs.writeFile("database2", JSON.stringify(mainData), () => { });
+    res.send(JSON.stringify(reqData));
+  });
+});
+app.get('/infoPrivate', (req, res) => {
+  fs.readFile("database2", 'utf8', (err, data) => {
+    const allData = JSON.parse(data);
+    res.send(JSON.stringify(allData.private))
+  })
+})
+app.post('/privateInfo', (req, res) => {
+  fs.readFile('database2', 'utf8', (err, data) => {
+    const allData = JSON.parse(data);
+    const reqData = req.body;
+    if (reqData.localname.length < 5) {
+      res.status(400).send(JSON.stringify({
+        error: "Name must be contain atleast 5 Characters"
+      }));
+      return;
+    };
+    if(reqData.nationality.length < 8) {
+      res.status(400).send(JSON.stringify({
+         error:"nationality must be contain atleast 8 Characters"
+      }));
+      return;
+    };
+    const rawImageString = reqData.image.replace(/^data:image\/jpeg;base64,/, "");
+    const buffer = Buffer.from(rawImageString, "base64");
+    reqData.id = allData.private.length + 1;
+    fs.writeFile(`public/empimage/${reqData.id}.jpeg`, buffer, () => { });
+    reqData.image = `${reqData.id}.jpeg`;
+    allData.private.push(reqData);
+    fs.writeFile("database2", JSON.stringify(allData), () => { });
+    res.send(JSON.stringify(allData));
+  });
 });
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
